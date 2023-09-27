93fa61a13884925b2e2d572e694e098e1469397f
Cooper Taylor <cooperjtaylor2005@gmail.com>
2023-09-27 12:21:30 -0400

    Modifications to header spacing algorithm and textbox centering

--- js/headerAlignment.js
+++ js/headerAlignment.js
@@ -1,21 +1,44 @@
-function spaceHeader(){
-    if (window.innerWidth >= 1050){
-            var width = window.innerWidth;
-            //0.1 for margin of left and right boxes
-            //second term for box width (105px);
-            var availableWidth = width - (2 * 0.05 * width + (3  * 105));
-            var margin = availableWidth / 4;
+function resetMargin(){
+        for(element of ["CuBox", "TaBox", "YBox"]) {
+                for(margin of ["margin-left","margin-right"]){
+                        if ((element == "CuBox" && margin == "margin-left") || (element == "YBox" && margin == "margin-right")){
+                                document.getElementById(element).style.setProperty(margin, "5vw");
+                        } else {
+                                document.getElementById(element).style.setProperty(margin, "15vw");
+                        }
+                }
+        }
+}
 
-            document.getElementById("CuBox").style.setProperty("margin-right",`${margin}px`);
-            document.getElementById("TaBox").style.setProperty("margin-left",`${margin}px`);
-            document.getElementById("TaBox").style.setProperty("margin-right",`${margin}px`);
-            document.getElementById("YBox").style.setProperty("margin-left",`${margin}px`);
-    } else {
-            document.getElementById("CuBox").style.setProperty("margin-right",`15vw`);
-            document.getElementById("TaBox").style.setProperty("margin-left",`15vw`);
-            document.getElementById("TaBox").style.setProperty("margin-right",`15vw`);
-            document.getElementById("YBox").style.setProperty("margin-left",`15vw`);
-    }
+function spaceHeader(){
+        if (window.innerWidth >= 1050){
+                var width = window.innerWidth;
+                //0.05 for margin of left and right boxes
+                //second term for box width (105px);
+                var availableWidth = width - (2 * 0.05 * width + (3  * 105));
+                var margin = availableWidth / 4;
+
+                document.getElementById("CuBox").style.setProperty("margin-right",`${margin}px`);
+                document.getElementById("TaBox").style.setProperty("margin-left",`${margin}px`);
+                document.getElementById("TaBox").style.setProperty("margin-right",`${margin}px`);
+                document.getElementById("YBox").style.setProperty("margin-left",`${margin}px`);
+        } else if (window.innerWidth >= 500) { //500 * 10vw = 50px (min-width of boxes)
+                resetMargin();
+        } else {
+                var width = window.innerWidth;
+                //50px for each box width
+                var availableWidth = width - (3 * 50);
+                
+                scaledWidth = availableWidth * (100/90) //Weighted widths are only 70% of total
+
+                //Change each margin with custom weight equivalent to view width
+                document.getElementById("CuBox").style.setProperty("margin-left",`${scaledWidth*0.15}px`);
+                document.getElementById("CuBox").style.setProperty("margin-right",`${scaledWidth*0.15}px`);
+                document.getElementById("TaBox").style.setProperty("margin-left",`${scaledWidth*0.15}px`);
+                document.getElementById("TaBox").style.setProperty("margin-right",`${scaledWidth*0.15}px`);
+                document.getElementById("YBox").style.setProperty("margin-left",`${scaledWidth*0.15}px`);
+                document.getElementById("YBox").style.setProperty("margin-right",`${scaledWidth*0.15}px`);
+        } 
 }
 
 window.addEventListener("resize", spaceHeader); 
